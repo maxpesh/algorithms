551c817629f125a0827b9bb281e6dc7696478e56
maxpesh <maxpeshkovit@gmail.com>
2025-06-20 06:40:06 -0400

    Make regular_div_with_rem denominator unsigned
    Ensures the denominator's range is consistent with natural numbers.

--- seminumerical/div_with_rem.c
+++ seminumerical/div_with_rem.c
@@ -2,19 +2,22 @@
 #include <math.h>
 #include <errno.h>
 #include <err.h>
+#include <limits.h>
 
 /*
- * a = b × q + r    a,q ∈ ℤ, b ∈ ℕ, 0 ≤ r < b;
- * q = ⌊a/b⌋
- * r = b × {a/b} 
+ * ∀a∈ℤ,b∈ℕ ∃!q,r∈ℤ: a=b×q+r; 0≤r<b;
+ * q=⌊a/b⌋, r=b×{a/b} 
  */
-div_t regular_div_with_rem(int a, int b)
+div_t regular_div_with_rem(int a, unsigned b)
 {
 	div_t dt;
+	int bs;
 
-	if (b <= 0)
-		errx(EINVAL, "The denominator should be >= 0, but was: %d", b);
-	dt.quot = (int)floor((double)a / b);
-	dt.rem = a - b * dt.quot;
+	if (b > INT_MAX)
+		errx(EXIT_FAILURE, "The denominator (%u) is out of range. "
+		    "Should be [0;%d]", b, INT_MAX);
+	bs = (int)b;
+	dt.quot = (int)floor((double)a / bs);
+	dt.rem = a - bs * dt.quot;
 	return dt;
 }
