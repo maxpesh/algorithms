74ae7a7da3dc644020f3072dc277afa983d56382
maxpesh <maxpeshkovit@gmail.com>
2025-06-21 04:17:21 -0400

    Add tests

--- seminumerical/div_with_rem.c
+++ seminumerical/div_with_rem.c
@@ -13,11 +13,42 @@ div_t regular_div_with_rem(int a, unsigned b)
 	div_t dt;
 	int bs;
 
-	if (b > INT_MAX)
+	if (b == 0 || b > INT_MAX)
 		errx(EXIT_FAILURE, "The denominator (%u) is out of range. "
-		    "Should be [0;%d]", b, INT_MAX);
+		    "Should be (0;%d]", b, INT_MAX);
 	bs = (int)b;
 	dt.quot = (int)floor((double)a / bs);
 	dt.rem = a - bs * dt.quot;
 	return dt;
+}
+
+/********************TESTS********************/
+#include <assert.h>
+
+static void test_regular_div_with_rem(void)
+{
+	div_t dt;
+
+	dt = regular_div_with_rem(8, 3);
+	assert(dt.quot == 2);
+	assert(dt.rem == 2);
+	dt = regular_div_with_rem(-8, 3);
+	assert(dt.quot == -3);
+	assert(dt.rem == 1);
+	dt = regular_div_with_rem(1, 2);
+	assert(dt.quot == 0);
+	assert(dt.rem == 1);
+	dt = regular_div_with_rem(-1, 2);
+	assert(dt.quot == -1);
+	assert(dt.rem == 1);
+	dt = regular_div_with_rem(0, 5);
+	assert(dt.quot == 0);
+	assert(dt.rem == 0);
+
+}
+
+int main(void)
+{
+	test_regular_div_with_rem();
+	return EXIT_SUCCESS;
 }
