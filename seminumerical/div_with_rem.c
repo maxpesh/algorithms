b6bd63f1234e660f659218b62d14e3e2e08bf569
maxpesh <maxpeshkovit@gmail.com>
2025-06-21 06:56:11 -0400

    Implement division, where the quotient is the nearest integer

--- seminumerical/div_with_rem.c
+++ seminumerical/div_with_rem.c
@@ -15,17 +15,17 @@ div_t regular_div_with_rem(int a, unsigned b)
 
 	if (b == 0 || b > INT_MAX)
 		errx(EXIT_FAILURE, "The denominator (%u) is out of range. "
-		    "Should be (0;%d]", b, INT_MAX);
+		    "Should be (0,%d]", b, INT_MAX);
 	bs = (int)b;
 	dt.quot = (int)floor((double)a / bs);
 	dt.rem = a - bs * dt.quot;
 	return dt;
 }
+
 /*
  * ∀a∈ℤ,b∈ℕ ∃!q,r∈ℤ: a=b×q-r; 0≤r<b;
- * q=⌈a/b⌉ 
+ * q=⌈a/b⌉
  */
-
 div_t div_with_neg_rem(int a, unsigned b)
 {
 	div_t dt;
@@ -33,13 +33,33 @@ div_t div_with_neg_rem(int a, unsigned b)
 
 	if (b == 0 || b > INT_MAX)
 		errx(EXIT_FAILURE, "The denominator (%u) is out of range. "
-		    "Should be (0;%d]", b, INT_MAX);
+		    "Should be (0,%d]", b, INT_MAX);
 	bs = (int)b;
 	dt.quot = (int)ceil((double)a / bs);
 	dt.rem = a - bs * dt.quot;
 	return dt;
-
 }	
+
+/*
+ * ∀a∈ℤ,b∈ℕ ∃!q,r∈ℤ: a=b×q+r
+ * ⎧r∈[-(b-1)/2,(b-1)/2], b-odd
+ * ⎨  
+ * ⎩r∈(-b/2,b/2], b-even
+ * q=⌈a/b-0.5⌉ 
+ */
+div_t div_nearest_int(int a, unsigned b)
+{
+	div_t dt;
+	int bs;
+
+	if (b == 0 || b > INT_MAX)
+		errx(EXIT_FAILURE, "The denominator (%u) is out of range. "
+		    "Should be (0,%d]", b, INT_MAX);
+	bs = (int)b;
+	dt.quot = (int)ceil(((double)a / bs) - 0.5);
+	dt.rem = a - bs * dt.quot;
+	return dt;
+}
 
 /********************TESTS********************/
 #include <assert.h>
@@ -86,9 +106,31 @@ static void test_div_with_neg_rem(void)
 	assert(dt.rem == 0);
 }
 
+static void test_div_nearest_int(void)
+{
+	div_t dt;
+
+	dt = div_nearest_int(8, 3);
+	assert(dt.quot == 3);
+	assert(dt.rem == -1);
+	dt = div_nearest_int(-8, 3);
+	assert(dt.quot == -3);
+	assert(dt.rem == 1);
+	dt = div_nearest_int(1, 2);
+	assert(dt.quot == 0);
+	assert(dt.rem == 1);
+	dt = div_nearest_int(-1, 2);
+	assert(dt.quot == -1);
+	assert(dt.rem == 1);
+	dt = div_nearest_int(0, 5);
+	assert(dt.quot == 0);
+	assert(dt.rem == 0);
+}
+
 int main(void)
 {
 	test_regular_div_with_rem();
 	test_div_with_neg_rem();
+	test_div_nearest_int();
 	return EXIT_SUCCESS;
 }
