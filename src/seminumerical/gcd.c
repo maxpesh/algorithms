45b63fae26395c6ede569baa70a8734cac69502b
maxpesh <maxpeshkovit@gmail.com>
2025-06-29 03:14:00 -0400

    Implement GCD algorithm using Euclidian division

--- src/seminumerical/gcd.c
+++ src/seminumerical/gcd.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <err.h>
+#include "div_with_rem.h"
 
 unsigned gcd_mod_op(int a, int b)
 {
@@ -13,6 +14,18 @@ unsigned gcd_mod_op(int a, int b)
 	return (unsigned)abs(a);
 }
 
+unsigned gcd_euclid(int a, int b)
+{
+	int r;
+
+	while (b != 0) {
+		r = euclidian_div(a, b).rem;
+		a = b;
+		b = r;
+	}
+	return (unsigned)abs(a);
+}
+
 #include <assert.h>
 
 static void test_gcd(unsigned (*gcdp)(int, int))
@@ -20,14 +33,18 @@ static void test_gcd(unsigned (*gcdp)(int, int))
 	int args[][2] = {
 	    {15,10}, {15,-10}, {-15,10}, {-15,-10},
 	    {10,15}, {10,-15}, {-10,15}, {-10,15},
+	    {4,2}, {4,-2}, {-4,2}, {-4,-2},
+	    {2,4}, {2,-4}, {-2,4}, {-2,-4},
 	    {13,8}, {13,-8}, {-13,8}, {-13,-8},
-	    {0,1729}, {1729,0}
+	    {0,1729}, {0, -1729}, {1729,0}, {-1729,0},
 	};
 	unsigned expect[] = {
 	    5, 5, 5, 5,
 	    5, 5, 5, 5,
+	    2, 2, 2, 2,
+	    2, 2, 2, 2,
 	    1, 1, 1, 1,
-	    1729, 1729
+	    1729, 1729, 1729, 1729,
 	};
 	size_t i = 0;
 
@@ -39,5 +56,6 @@ static void test_gcd(unsigned (*gcdp)(int, int))
 int main(void)
 {
 	test_gcd(gcd_mod_op);
+	test_gcd(gcd_euclid);
 	return EXIT_SUCCESS;
 }
